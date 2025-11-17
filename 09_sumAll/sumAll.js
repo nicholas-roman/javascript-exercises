cbaf38da29819e23c501d445970d3c09ec58ad0b
Nicholas Roman <nroman@uwaterloo.ca>
2025-11-17 11:32:19 -0500

    Worked on exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    let start = Math.min(n1, n2)
+    let end = Math.max(n1, n2)
+    res = 0
 
+    for (let i = start; i <= end; i++) {
+        res += i
+    }
+
+    return res
 };
 
 // Do not edit below this line
